16df4aae72fb9fd4aff33b89099def63054eeb29
JavaEEPRO <j2eengineer@gmail.com>
2019-11-09 20:22:09 +0200

    first test falls ( CollectorServiceTest::getSnapshot_whenReceivedListIsNotEmpty_thenCorrect ) expecting, that CollectorService should return not empty list with data

--- src/test/java/si/inspirited/CollectorServiceTest.java
+++ src/test/java/si/inspirited/CollectorServiceTest.java
@@ -1,8 +1,29 @@
 package si.inspirited;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import si.inspirited.service.ICollectorService;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class CollectorServiceTest {
+
+    @Autowired
+    ICollectorService collectorService;
+
+    @Test
+    public void getSnapshot_whenReceivedListIsNotEmpty_thenCorrect() {
+        List<Object> expectedDataFromInputStream = collectorService.getSnapshot();
+
+        assertNotNull(expectedDataFromInputStream);
+        assertFalse(expectedDataFromInputStream.isEmpty());
+    }
 }
