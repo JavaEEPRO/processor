db30e700592b078fd5cb78b8bfd51a12833fae2b
JavaEEPRO <j2eengineer@gmail.com>
2019-11-09 20:09:14 +0200

    added junit 4 to classpath; init Collector Service Test

---
+++ src/test/java/si/inspirited/CollectorServiceTest.java
@@ -0,0 +1,8 @@
+package si.inspirited;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class CollectorServiceTest {
+}
