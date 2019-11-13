f144f37aaa1c8c4addfd9f5fb07b15bb024d9c00
JavaEEPRO <j2eengineer@gmail.com>
2019-11-13 16:05:04 +0200

    6-th test is up ( AdvancedControllerServiceTests::getExtendedCompanySnapshot_withFeedingNullInsteadOfStockCode_whenThrownInvalidStockCodeException_thenCorrect ) when ACS get... method receives null instead of correct stock code, then it throws InvalidStockCodeException, as expected

--- src/main/java/si/inspirited/service/impl/AdvancedCollectorService.java
+++ src/main/java/si/inspirited/service/impl/AdvancedCollectorService.java
@@ -2,6 +2,7 @@ package si.inspirited.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import si.inspirited.error.InvalidStockCodeException;
 import si.inspirited.service.IAdvancedCollectorService;
 import si.inspirited.service.ICollectorService;
 
@@ -15,6 +16,7 @@ public class AdvancedCollectorService implements IAdvancedCollectorService {
 
     @Override
     public Object getExtendedCompanySnapshot(String url, String stockCode) {
+        if (stockCode == null || "".equals(stockCode.trim())) { throw new InvalidStockCodeException(); }
         String formattedUrl = url != null ? url.replace("{stockCode}", stockCode) : "";
         List<Object> data = collectorService.getSnapshot(formattedUrl);
         if (data.size() == 1) { return data.get(0); }
