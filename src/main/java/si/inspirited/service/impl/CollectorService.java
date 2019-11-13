d1fc0d23b41e585e8cc6a20fdee80913da4bde90
JavaEEPRO <j2eengineer@gmail.com>
2019-11-13 12:33:50 +0200

    fourth test succeeded ( CollectorServiceTest::feedObviouslyIncorrectDataIntoReadJsonFromInputStream_whenThrowsUnexpectedDataFormatException_thenCorrect ) as expected, when incorrect data is received, UnexpectedReceivedDataFormatException then being thrown

--- src/main/java/si/inspirited/service/impl/CollectorService.java
+++ src/main/java/si/inspirited/service/impl/CollectorService.java
@@ -3,6 +3,7 @@ package si.inspirited.service.impl;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.springframework.stereotype.Service;
+import si.inspirited.error.UnexpectedReceivedDataFormatException;
 import si.inspirited.service.ICollectorService;
 
 import java.io.*;
@@ -24,16 +25,23 @@ public class CollectorService implements ICollectorService {
         return json.toList();
     }
     
-    private JSONArray readJsonFromUrl(final String url) throws IOException, JSONException {
+    private JSONArray readJsonFromUrl(final String url) throws IOException {
         try (InputStream inputStream = new URL(url).openStream()) {
             return readJsonFromInputStream(inputStream);
         }
     }
 
-    public JSONArray readJsonFromInputStream(final InputStream inputStream) throws IOException{
+    public JSONArray readJsonFromInputStream(final InputStream inputStream) throws IOException, UnexpectedReceivedDataFormatException{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String jsonText = readAll(bufferedReader);
-        JSONArray json = new JSONArray(jsonText);
+        JSONArray json = new JSONArray();
+        try {
+            json = new JSONArray(jsonText);
+        } catch (JSONException jsonException) {
+            UnexpectedReceivedDataFormatException unexpectedReceivedDataFormatException = new UnexpectedReceivedDataFormatException(jsonException.getMessage(), jsonException.getCause());
+            unexpectedReceivedDataFormatException.setStackTrace(jsonException.getStackTrace());
+            throw unexpectedReceivedDataFormatException;
+        }
         return json;
     }
 
