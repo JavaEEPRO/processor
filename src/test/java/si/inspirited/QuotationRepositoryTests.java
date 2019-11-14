8e416f559a0e6eee0c795b459bb88773101bb37d
JavaEEPRO <j2eengineer@gmail.com>
2019-11-14 09:21:28 +0200

    refactoring: 've incapsulated unnecessary class field

--- src/test/java/si/inspirited/QuotationRepositoryTests.java
+++ src/test/java/si/inspirited/QuotationRepositoryTests.java
@@ -21,29 +21,29 @@ public class QuotationRepositoryTests {
     @Autowired
     QuotationRepository quotationRepository;
 
-    private Quotation STUB_QUOTATION = new Quotation();
-
     @Test
     public void pushQuotationToRepo_ifCouldFindItStored_thenCorrect() {
-        Quotation returnedQuotation = quotationRepository.save(getStubQuotation());
+        Quotation quotationForPush = getStubQuotation();
+        Quotation returnedQuotation = quotationRepository.save(quotationForPush);
         assertNotNull(returnedQuotation);
         Long id = returnedQuotation.getId();
         assertNotNull(id);
         returnedQuotation.setId(null);
-        assertEquals(STUB_QUOTATION, returnedQuotation);
+        assertEquals(quotationForPush, returnedQuotation);
         Quotation queriedFromRepoQuotation = quotationRepository.getOne(id);
         assertEquals(id, queriedFromRepoQuotation.getId());
     }
 
     Quotation getStubQuotation() {
-        STUB_QUOTATION.setSymbol("MRCY");
-        STUB_QUOTATION.setCompanyName("Microsauce");
-        STUB_QUOTATION.setPrimaryExchange("PARAUS");
-        STUB_QUOTATION.setCalculationPrice("rise");
-        STUB_QUOTATION.setOpen("of course");
-        STUB_QUOTATION.setOpenTime(LocalTime.of(11,12));
-        STUB_QUOTATION.setLatestPrice(352.347);
-        STUB_QUOTATION.setChangePercent(-0.0004);
-        return STUB_QUOTATION;
+        Quotation res = new Quotation();
+        res.setSymbol("MRCY");
+        res.setCompanyName("Microsauce");
+        res.setPrimaryExchange("PARAUS");
+        res.setCalculationPrice("rise");
+        res.setOpen("of course");
+        res.setOpenTime(LocalTime.of(11,12));
+        res.setLatestPrice(352.347);
+        res.setChangePercent(-0.0004);
+        return res;
     }
 }
