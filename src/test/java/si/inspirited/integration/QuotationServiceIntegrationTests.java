492376b5beafd0ca864f2b27fb6cd1f50d768fc2
JavaEEPRO <j2eengineer@gmail.com>
2019-11-14 13:49:20 +0200

    11-th test falls down ( QuotationServiceIntegrationTests::addDozenQuotations_whenReceivedListHasOnlyLastFiveQuotationsOrderedByChangePercentDesc_thenCorrect ) when dozen quotations are added, then expecting to get their last 5 (ordered by changePercent descending) thru the method getLast5QuotationsOrderedDescByChangePercent

--- src/test/java/si/inspirited/integration/QuotationServiceIntegrationTests.java
+++ src/test/java/si/inspirited/integration/QuotationServiceIntegrationTests.java
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import si.inspirited.persistence.dao.QuotationRepository;
 import si.inspirited.persistence.model.Quotation;
+import si.inspirited.service.IQuotationService;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,25 +20,52 @@ import static org.junit.jupiter.api.Assertions.*;
 public class QuotationServiceIntegrationTests {
 
     @Autowired
-    QuotationRepository quotationRepository;
+    IQuotationService quotationService;
 
     @Test
     public void addQuotation_whenReceivedListContainsIt_thenCorrect() {
         Quotation quotationToSave = getStubQuotation();
-        Quotation quotationReturned = quotationRepository.save(quotationToSave);
+        Quotation quotationReturned = quotationService.addQuotation(quotationToSave);
         Long id = quotationReturned.getId();
         assertNotNull(quotationReturned);
         assertNotEquals(id, null);
         quotationReturned.setId(null);
         assertEquals(quotationToSave, quotationReturned);
-        List<Quotation> receivedList = quotationRepository.findTopOrderedByLatestPrice();
+        List<Quotation> receivedList = quotationService.getTop5QuotationsOrderedByLatestPrice();
         assertEquals(1, receivedList.size());
         Quotation quotationQueried = receivedList.get(0);
         quotationQueried.setId(null);
         assertEquals(quotationToSave, quotationQueried);
     }
 
+    @Test
+    public void addDozenQuotations_whenReceivedListHasOnlyLastFiveQuotationsOrderedByChangePercentDesc_thenCorrect() {
+        List<Quotation> quotationsBeenAdded = addAndGetDozenOfQuotation();
+        List<Quotation> quotationsBeenQueried = quotationService.getLast5QuotationsOrderedDescByChangePercent();
+        assertEquals(5, quotationsBeenQueried.size());
+        for (int i = 0; i < quotationsBeenQueried.size() - 1; i++) {
+            Double thisQuotationLatestPrice = quotationsBeenQueried.get( i ).getChangePercent();
+            Double nextQuotationLatestPrice = quotationsBeenQueried.get( i + 1 ).getChangePercent();
+            assertTrue(thisQuotationLatestPrice < nextQuotationLatestPrice);
+        }
+    }
+
     //
+    private List<Quotation> addAndGetDozenOfQuotation() {
+        List<Quotation> res = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            Quotation nextQuotation = getStubQuotation();
+            Random random = new Random();
+            Double latestPrice = nextQuotation.getLatestPrice() * random.nextDouble();
+            Double changePercent = nextQuotation.getChangePercent() * random.nextDouble();
+            nextQuotation.setLatestPrice(latestPrice);
+            nextQuotation.setChangePercent(changePercent);
+            nextQuotation = quotationService.addQuotation(nextQuotation);
+            res.add(nextQuotation);
+        }
+        return res;
+    }
+
     private Quotation getStubQuotation() {
         Quotation res = new Quotation();
         res.setSymbol("PRFCT");
@@ -44,7 +73,7 @@ public class QuotationServiceIntegrationTests {
         res.setPrimaryExchange("PREX");
         res.setCalculationPrice("highest");
         res.setOpen("any time");
-        res.setOpenTime(LocalTime.of(01,02));
+        res.setOpenTime(LocalTime.of(01,02, 03, 003));
         res.setLatestPrice(213.731);
         res.setChangePercent(-0.0007);
         return res;
