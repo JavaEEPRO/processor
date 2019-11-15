2457b5e6bb72ab74ff5e63fa0dff44ae26d75a2d
JavaEEPRO <j2eengineer@gmail.com>
2019-11-15 16:34:59 +0200

    13-th test picked up ( EnabledStatusFilter::getEnabledIsCalledWithCoupleObjectsAreFeedInThere_whenReturnedListContainsOnlyEnabled_thenCorrect ) implemented filter, so, when ESF get... method has on its input list with enabled and disabled records, then it returns only enabled, as expected

--- src/main/java/si/inspirited/service/impl/EnabledStatusFilter.java
+++ src/main/java/si/inspirited/service/impl/EnabledStatusFilter.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 import si.inspirited.dto.RowDto;
 import si.inspirited.service.IEnabledStatusFilter;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
@@ -11,6 +14,43 @@ public class EnabledStatusFilter implements IEnabledStatusFilter {
 
     @Override
     public List<RowDto> getEnabled(List<? extends Object> toFilter) {
-        return null;
+        List<RowDto> res = new ArrayList<>();
+        if (toFilter == null || toFilter.isEmpty()) {
+            return res;
+        }
+        for (Object o : toFilter) {
+            HashMap<String, Object> mapped = (HashMap<String, Object>) o;
+            String symbol = "";
+            Boolean enabled = false;
+            String exchange = "";
+            String name = "";
+            LocalDate date = null;
+            if (mapped.containsKey("symbol")) {
+                symbol = (String) mapped.get("symbol");
+            }
+            if (mapped.containsKey("isEnabled")) {
+                enabled = (Boolean) mapped.get("isEnabled");
+            }
+            if (mapped.containsKey("exchange")) {
+                exchange = (String) mapped.get("exchange");
+            }
+            if (mapped.containsKey("name")) {
+                name = (String) mapped.get("name");
+            }
+            if (mapped.containsKey("date")) {
+                date = (LocalDate) mapped.get("date");
+            }
+                if (enabled) {
+                    RowDto row = new RowDto();
+                    row.setSymbol(symbol);
+                    row.setEnabled(enabled);
+                    row.setExchange(exchange);
+                    row.setName(name);
+                    row.setDate(date);
+
+                    res.add(row);
+                }
+            }
+            return res;
+        }
     }
-}
