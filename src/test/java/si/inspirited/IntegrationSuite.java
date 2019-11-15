b1ac5a408eed511b59ba07e56746f16fe56483c2
JavaEEPRO <j2eengineer@gmail.com>
2019-11-15 16:40:00 +0200

    refactoring: restructured Integration Suite

--- src/test/java/si/inspirited/IntegrationSuite.java
+++ src/test/java/si/inspirited/IntegrationSuite.java
@@ -7,11 +7,12 @@ import si.inspirited.integration.QuotationServiceIntegrationTests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        CustomPropertiesIntegrationTest.class,
         CollectorServiceTests.class,
         AdvancedCollectorServiceTests.class,
+        EnabledStatusFilterTests.class,
         QuotationRepositoryTests.class,
-        QuotationServiceIntegrationTests.class,
-        CustomPropertiesIntegrationTest.class,
+        QuotationServiceIntegrationTests.class
 })
 public class IntegrationSuite {
 }
