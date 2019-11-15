ddf643b5a03d290ab0550bd26024067e2d793061
JavaEEPRO <j2eengineer@gmail.com>
2019-11-15 10:39:51 +0200

    added new type Snapshot for using instead of Object that comes from Collector Service

---
+++ src/main/java/si/inspirited/dto/SnapshotDto.java
@@ -0,0 +1,54 @@
+package si.inspirited.dto;
+
+import java.time.LocalDate;
+
+public class SnapshotDto {
+
+    private String symbol;
+    private boolean enabled;
+
+    private String exchange;
+    private String name;
+    private LocalDate date;
+
+    //
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
