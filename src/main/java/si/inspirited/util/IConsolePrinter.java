1c186c1c857a4730e379ae151ad5531a152d7517
JavaEEPRO <j2eengineer@gmail.com>
2019-11-16 23:13:39 +0200

    added Console Printer for making output to the console every 5 sec

---
+++ src/main/java/si/inspirited/util/IConsolePrinter.java
@@ -0,0 +1,8 @@
+package si.inspirited.util;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public interface IConsolePrinter extends Runnable {
+
+    void startPrintingConclusions();
+}
---
+++ src/main/java/si/inspirited/util/impl/ConsolePrinter.java
@@ -0,0 +1,31 @@
+package si.inspirited.util.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import si.inspirited.service.IQuotationService;
+import si.inspirited.util.IConsolePrinter;
+
+@Component
+public class ConsolePrinter implements IConsolePrinter {
+
+    @Autowired
+    IQuotationService quotationService;
+
+    @Override
+    public void startPrintingConclusions() {
+        run();
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            System.out.println(quotationService.getTop5QuotationsOrderedByLatestPrice4LastOfThemByCompanyName());
+            System.out.println(quotationService.getTop5QuotationsOrderedByChangePercent());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
